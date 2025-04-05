31f069d5d92c741719c61e806c3fa1242aca7777
loverboy <loverboykosu@gmail.com>
2025-04-05 09:35:28 +0900

    refactor: fix file construction

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import allReducers from "./reducers/index.js";
+import { createStore } from "redux";
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
