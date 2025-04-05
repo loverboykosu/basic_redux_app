fda5eea2aea0042a8ccbcc86397224b26fb0ccbf
loverboy <loverboykosu@gmail.com>
2025-04-05 09:28:18 +0900

    feat: delete isLogin.js

--- src/reducers/isLogin.js
+++
@@ -1,9 +0,0 @@
-const isLoginReducer = (state = 0, action) => {
-  switch (action.type) {
-    case "LOGIN":
-      return !state;
-    default:
-      return state;
-  }
-};
-export default isLoginReducer;
